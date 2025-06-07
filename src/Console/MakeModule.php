082557ca3767571fbf7be2992a51d3a9509dd34e
syderbit <syderbit182@gmail.com>
2025-06-08 00:29:58 +0700

    update

--- src/Console/MakeModule.php
+++ src/Console/MakeModule.php
@@ -10,10 +10,11 @@ class MakeModule extends Command
 {
     protected $signature = 'make:module-lmm {name}';
     protected $description = 'Generate a new module under resources/modules with Laravel Module Management';
- 
+
     public function handle()
     {
         $name = Str::studly($this->argument('name'));
+        $className = Str::studly($this->argument('name'));
         $basePath = resource_path("modules/{$name}");
 
         $paths = [
@@ -22,12 +23,37 @@ class MakeModule extends Command
             "{$basePath}/resources/views",
         ];
 
+
         foreach ($paths as $path) {
             File::makeDirectory($path, 0755, true, true);
         }
 
-        File::put("{$basePath}/resources/views/index.blade.php", "<h1>Module {$name}</h1>");
+        //View
+        $nameLower = Str::lower($name);
+        File::put("{$basePath}/resources/views/{$nameLower}.blade.php", "<h1>Module {$name}</h1>");
+        File::put("{$basePath}/resources/assets/js/{$nameLower}.js", "");
 
+
+        // Buat controller dari stub
+        $stubPath = __DIR__ . '/../../stubs/ModuleController.stub';
+        $controllerContent = str_replace(
+            [
+                'DummyNamespace',
+                'DummyClass',
+                'DummyView',
+                'DummyViewName',
+            ],
+            [
+                "Modules\\{$className}\\Http\\Controllers",
+                "{$className}Controller",
+                $name,
+                $nameLower,
+            ],
+            file_get_contents($stubPath)
+        );
+
+        //Module
+        File::put("{$basePath}/app/Http/Controllers/{$className}Controller.php", $controllerContent);
         $this->info("Module '{$name}' created in resources/modules/{$name}");
     }
 }
